c4e0330e5a9eb02b2709d5c40af7c1beea90f817
Glen Danielle D. Galit <glendanielleg@gmail.com>
2024-07-05 19:13:31 +0800

    created a initial db

---
+++ backEnd/Data/Migrations/20240705111145_CreateInitial.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BackEnd.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OrderName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerId = table.Column<string>(type: "TEXT", nullable: false),
+                    DateOrder = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    TotalPrice = table.Column<double>(type: "REAL", nullable: false),
+                    OrderInfoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_OrderInfos_OrderInfoId",
+                        column: x => x.OrderInfoId,
+                        principalTable: "OrderInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "OrderInfos",
+                columns: new[] { "Id", "OrderName" },
+                values: new object[,]
+                {
+                    { 1, "Fried Siken" },
+                    { 2, "Fried Siomai" },
+                    { 3, "Fried Siopao" },
+                    { 4, "Fried Siokoy" },
+                    { 5, "Fried Okoy" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderInfoId",
+                table: "Orders",
+                column: "OrderInfoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "OrderInfos");
+        }
+    }
+}
