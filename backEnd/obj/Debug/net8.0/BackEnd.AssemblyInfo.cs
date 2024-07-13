98bdf7fe3bf0b872240de1368bc0d3cd45adc5ed
Glen Danielle D. Galit <glendanielleg@gmail.com>
2024-07-13 22:04:49 +0800

    unfinished setting up cors and axios

--- backEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ backEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0141e6790d22b8806c0fd7934bc1b18bb768a527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2f8604076a10217e8cbc13ce64ff9e31f809cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
